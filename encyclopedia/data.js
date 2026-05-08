02ad829479767cbec8cf12cb34097123b7c7815c
PiR-Du <p.duchesne74@gmail.com>
2026-05-08 11:18:28 +0200

    new infos

--- encyclopedia/data.js
+++ encyclopedia/data.js
@@ -1,5 +1,6 @@
 const INITIAL_DATA = {
   "infos": [
-    { "title": "Omelette", "text": "Pour faire une omelette, il y a besoin de plus qu'un oeuf par personne (contrairement aux oeufs aux plats) #Axiome", "category": "Cuisine", "source": "Julien" },
+    { "title": "Omelette", "text": "Pour faire une omelette, il y a besoin de plus qu'un oeuf par personne (contrairement aux oeufs aux plats)", "category": "Cuisine", "source": "Julien" },
+    { "title": "Vilipender", "text": "Vilipender : traiter quelqu'un avec mépris", "category": "Vocabulaire", "source": "Crepu" },
   ]
 };
